963e69e23b83d49fc4e123538016275d95aaad27
Patrick Neff <odie86@gmail.com>
2023-07-17 13:59:19 +0200

    Add alpha patch

--- config.def.h
+++ config.def.h
@@ -3,6 +3,8 @@
 
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
 static const unsigned int alpha = 0xff;     /* Amount of opacity. 0xff is opaque             */
+static int fuzzy = 1;                      /* -F  option; if 0, dmenu doesn't use fuzzy matching     */
+
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
 	"monospace:size=10"
